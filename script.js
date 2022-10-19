b7688f90c35b217d5670a10832853dcadb37b3f2
HAmid <hamidprince823@gmail.com>
2022-10-19 13:21:40 +0500

    completed

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 let bookmark = document.querySelector("#bookmark");
-let bookmarClicked = true;
+let clicked = true;
 function myFunc() {
-  if (bookmarClicked) {
-    bookmarClicked = false;
+  if (clicked) {
+    clicked = false;
     bookmark.innerHTML = "Bookmarked";
     document.getElementById("pop-up").style.display = "flex";
     document.getElementById("bookmark-container").style.color = "gray";
     document.getElementById("icon").style.backgroundColor = "gray";
   } else {
-    bookmarClicked = true;
+    clicked = true;
     bookmark.innerHTML = "Bookmark";
     document.getElementById("bookmark-container").style.color = "green";
     document.getElementById("icon").style.backgroundColor =
       "hsl(176, 72%, 28%)";
   }
 }
-// function bookmarkFunc() {
-//   if (bookmark.innerHTML === "Bookmark") {
-//     bookmark.innerHTML = "Bookmarked ";
-//     document.getElementById("pop-up").style.display = "flex";
-//     document.getElementById("bookmark-container").style.color = "gray";
-//     document.getElementById("icon").style.backgroundColor = "gray";
-//   } else {
-//     bookmark.innerHTML = "Bookmark";
-//     document.getElementById("bookmark-container").style.color = "green";
-//     document.getElementById("icon").style.backgroundColor =
-//       "hsl(176, 72%, 28%)";
-//   }
-// }
+
 function popOut() {
   document.getElementById("pop-up").style.display = "none";
+}
+// Pledge  //
+function backto() {
+  document.getElementById("plegde").style.display = "block";
+}
+
+function cancelOut() {
+  document.getElementById("plegde").style.display = "none";
+}
+//  pledgeText //
+
+function funding(price,check) {
+  if (clicked) {
+    clicked = false;
+    document.querySelector(price).style.display = "flex";
+    document.querySelector(price).classList.add("pricinSection");
+    document.querySelector(check).style.backgroundColor = "hsl(176, 72%, 28%)";
+    document.querySelector(check).style.borderColor = "white";
+  } else {
+    clicked = true;
+    document.querySelector(price).style.display = "none";
+    document.querySelector(check).style.backgroundColor = "white";
+    document.querySelector(check).style.borderColor = "green";
+  }
 }
